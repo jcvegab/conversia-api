5099add1f42cd84a5194b4e4bf5414193384bbe8
Joseph Vega <jcvb0206@gmail.com>
2024-03-16 16:41:27 -0500

    Setup Prisma Client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { join } from 'path';
 
+import { PrismaService } from './prisma/prisma.service';
+
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
   ],
+  providers: [PrismaService],
 })
 export class AppModule {}
